906d2071ac624f8418e4d51802132752af3a4d63
Vinz <vinzkarloemendoza@gmail.com>
2019-05-21 17:06:17 +0800

    Updates

---
+++ precache-manifest.85934836be0c5fe2f461e99258967305.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777dfdf731e5172cc5d7eb4ef266d5c2",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "94b22c1f9da9d5aae87f",
+    "url": "/redux-todo/static/js/2.369629de.chunk.js"
+  },
+  {
+    "revision": "b0e8a7e57a4a4d3db2e9",
+    "url": "/redux-todo/static/js/main.3c2a8445.chunk.js"
+  },
+  {
+    "revision": "4a2d5658e1b8a4be0a5f",
+    "url": "/redux-todo/static/js/runtime~main.134d15df.js"
+  }
+]);
